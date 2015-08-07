094185f3184d3a8b6011d955a57e7d39ff692664
pazhan <pazhan@microsoft.com>
2015-08-07 12:42:32 -0700

    modularized?

--- scrpt.js
+++ scrpt.js
@@ -1,50 +1,52 @@
-let script1 = (function iife1() {
+var script1 = (function () {
 
-let parity = 1;
-function clickfunc()
-{
-	let elem = document.getElementById("img1");
-	if(parity%2 == 1)
+	let parity = 1;
+	let clickfunc = function ()
 	{
-		elem.src = "orange.jpg";
-		elem.style.width = "400px";
-		elem.style.height = "400px";
-		
+		let elem = document.getElementById("img1");
+		if(parity%2 == 1)
+		{
+			elem.src = "orange.jpg";
+			elem.style.width = "400px";
+			elem.style.height = "400px";
+			
+		}
+		else
+		{
+			elem.src = "apple.jpg";
+			elem.style.width = "100px";
+			elem.style.height = "100px";
+		}
+		parity++;
 	}
-	else
+
+	function writecookie()
 	{
-		elem.src = "apple.jpg";
-		elem.style.width = "100px";
-		elem.style.height = "100px";
+		var cname = prompt("name");
+		var cvalue = prompt("value");
+		setCookie(cname,cvalue,5);
 	}
-	parity++;
-}
 
-function writecookie()
-{
-	var cname = prompt("name");
-	var cvalue = prompt("value");
-	setCookie(cname,cvalue,5);
-}
+	function setCookie(cname, cvalue, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		var expires = "expires="+d.toUTCString();
+		document.cookie = cname + "=" + cvalue + "; " + expires;
+	} 
 
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
-} 
+	function showcookie()
+	{
+		//alert(document.cookie);
+		let elem = document.getElementById("cookies");
+		elem.innerHTML = document.cookie;
+	}
 
-function showcookie()
-{
-	alert(document.cookie);
-	let elem = document.getElementById("coockies");
-	elem.innerHTML = document.cookie;
-}
+	function deletecookie()
+	{
+		var cname = prompt("name");
+		setCookie(cname,"blah",-5);
+	}
 
-function deletecookie()
-{
-	var cname = prompt("name");
-	setCookie(cname,"blah",-5);
-}
+	return {clickfunc, writecookie, setCookie, showcookie, deletecookie};
 
 }() )
